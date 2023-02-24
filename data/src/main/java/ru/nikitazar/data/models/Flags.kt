73ad025a9a99da51baaa0f56c6a80fe06771c571
NikitaZar <webbizon@ya.ru>
2023-02-24 22:19:34 +0400

    fix bug in data class Flags (data)

--- data/src/main/java/ru/nikitazar/data/models/Flags.kt
+++ data/src/main/java/ru/nikitazar/data/models/Flags.kt
@@ -4,10 +4,10 @@ import com.google.gson.annotations.SerializedName
 
 data class Flags(
     @SerializedName("html") val html: Int = 0,
-    @SerializedName("no_value") val noValue: Boolean = false,
-    @SerializedName("no_name") val noName: Boolean = false,
-    @SerializedName("no_image") val noImage: Boolean = false,
-    @SerializedName("no_wrap") val noWrap: Boolean = false,
-    @SerializedName("no_wrap_name") val noWrapName: Boolean = false,
-    @SerializedName("system") val system: Boolean? = false
+    @SerializedName("no_value") val noValue: Int = 0,
+    @SerializedName("no_name") val noName: Int = 0,
+    @SerializedName("no_image") val noImage: Int = 0,
+    @SerializedName("no_wrap") val noWrap: Int = 0,
+    @SerializedName("no_wrap_name") val noWrapName: Int = 0,
+    @SerializedName("system") val system: Int? = 0
 )
